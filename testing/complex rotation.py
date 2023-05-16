c51af93344b733877ad0f0a00dd49f60cbe378dd
niko <91669604+qtniko@users.noreply.github.com>
2023-05-16 21:53:38 +0200

    Update complex rotation.py
    comments

--- testing/complex rotation.py
+++ testing/complex rotation.py
@@ -4,9 +4,9 @@ Easy rotation using complex numbers
 
 import numpy as np
 
-P = complex(1, 2)
+P = complex(1, 2) # Point(1, 2)
 
-Q = P * np.exp(np.pi*complex(0, 1))
+Q = P * np.exp(np.pi*complex(0, 1)) # Rotate P π radians ccw about the origin
 
 Q = complex(round(np.real(Q), 10), round(np.imag(Q), 10)) # Rounding Q cuz oogli boogli
 
