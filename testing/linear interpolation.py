92bf9d35bfd5cc63d33b023fc2ac9064182aac4a
niko <91669604+qtniko@users.noreply.github.com>
2023-06-01 11:06:34 +0200

    Update linear interpolation.py

--- testing/linear interpolation.py
+++ testing/linear interpolation.py
@@ -4,15 +4,13 @@ Using linear interpolation to find y=0
 
 import numpy as np
 
-def lerp(a: float, b: float, t: float):
+def lerp(a, b, t):
     return (1 - t) * a + b * t
 
-P1 = np.array([1, 2], dtype=float)
-P2 = np.array([4, -5], dtype=float)
-y1 = P1[1]
-y2 = abs(P2[1])
+A = np.array([1, 2], dtype=float)
+B = np.array([4, -5], dtype=float)
 
-t = y1 / (y2 + y1)
-P = lerp(P1, P2, t)
+t = A[1] / abs(B[1] - A[1])
+P = lerp(A, B, t)
 
 print(tuple(P))
